a93e14f0462387e79cc62168cda80420132d186b
Fran Martinez <fj.martinez.paez@gmail.com>
2023-07-26 14:47:27 +0400

    init

---
+++ app-vectors.py
@@ -0,0 +1,51 @@
+import streamlit as st
+
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
+from langchain.vectorstores import FAISS
+
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+
+def get_vectorstore(text_chunks):
+    # embeddings = OpenAIEmbeddings()
+    embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    return vectorstore
+
+
+def main():
+    load_dotenv()
+
+    st.set_page_config(page_title="Chat with multiple PDFs")
+    st.header("Chat with multiple PDFs :books:")
+
+    # raw_text = get_pdf_text(pdf_docs)
+    raw_text = "Test text for summary"
+
+    text_chunks = get_text_chunks(raw_text)
+
+    vectorstore = get_vectorstore(text_chunks)
+
+if __name__ == '__main__':
+    main()
